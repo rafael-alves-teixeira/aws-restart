2193cf476649f6e650e20ba269e9e7371d7c138c
Rafael Alves <rafael.info.tc@gmail.com>
2024-01-05 16:14:03 -0300

    add exercises

---
+++ my-collections.py
@@ -0,0 +1,25 @@
+myFruitList = ["Apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] =  "orange"
+print(myFruitList)
+myFinalAnswerTuple = ("Apple", "Banana", "Pineaple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary = {
+    "Akua" : "apple",
+    "Saanvi" : "banana",
+    "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
---
+++ string-data-type.py
@@ -0,0 +1,13 @@
+myString="This is my string"
+print(myString)
+print(type(myString))
+print(myString + "is of the data type" + str(type(myString)))
+firstString="water"
+secondString="fall"
+thirdString= firstString + secondString
+print(thirdString)
+name = input("What is your name? ")
+print(name)
+color = input("What is your favourite color?  ")
+animal = input("What is your favourite animal?  ")
+print("{}, you like a {} {}!".format(name, color, animal))
